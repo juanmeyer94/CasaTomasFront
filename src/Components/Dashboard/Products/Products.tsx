cd8827cd991e0f1cf99f31fe36552144e3941b33
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-08-25 10:41:03 -0300

    fixs

--- src/Components/Dashboard/Products/Products.tsx
+++ src/Components/Dashboard/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import useUserContext from "../../../Utils/contextUserHook";
 import { ObjectType } from "../../../Interfaces/interfacesIndex";
 import SearchBar from "../../SearchBar/searchBar";
@@ -18,13 +18,45 @@ const Products: React.FC = () => {
     setPostsPerPage,
   } = useUserContext();
 
-  const {isAuth} = useAdminContext();
+  const { isAuth } = useAdminContext();
 
   if (loading) {
     return (
       <div className="flex rounded-xl items-center justify-center flex-col p-8 font-bold text-2xl bg-sky-100 h-screen">
         <h1>Cargando...</h1>
-        <img src="/giphy.webp" alt="" className="rounded-full max-h-[220px] 2xl:max-h-[320px]" />
+        <svg
+          width="50"
+          height="50"
+          viewBox="0 0 50 50"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="25"
+            cy="25"
+            r="20"
+            stroke="gray"
+            stroke-width="5"
+            fill="none"
+          />
+          <circle
+            cx="25"
+            cy="25"
+            r="20"
+            stroke="blue"
+            stroke-width="5"
+            stroke-dasharray="126"
+            stroke-dashoffset="0"
+            fill="none"
+          >
+            <animate
+              attributeName="stroke-dashoffset"
+              values="0;126"
+              dur="1.5s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </svg>
+
         <p>Casa Tomas - 100 años cociendo juntos.</p>
       </div>
     );
@@ -34,28 +66,28 @@ const Products: React.FC = () => {
 
   useEffect(() => {
     const updateItemsPerPage = () => {
-        if (window.innerWidth >= 1736) {
-            setPostsPerPage(10); // 2xl
-        } else if (window.innerWidth >= 1500) {
-            setPostsPerPage(10); // xl
-        } else if (window.innerWidth >= 1280) {
-            setPostsPerPage(10); // lg
-        } else if (window.innerWidth >= 1025) {
-            setPostsPerPage(6); // default
-        } else {
-            setPostsPerPage(4); // sm
-        }
+      if (window.innerWidth >= 1736) {
+        setPostsPerPage(10); // 2xl
+      } else if (window.innerWidth >= 1500) {
+        setPostsPerPage(10); // xl
+      } else if (window.innerWidth >= 1280) {
+        setPostsPerPage(10); // lg
+      } else if (window.innerWidth >= 1025) {
+        setPostsPerPage(6); // default
+      } else {
+        setPostsPerPage(4); // sm
+      }
     };
 
-    window.addEventListener('resize', updateItemsPerPage);
+    window.addEventListener("resize", updateItemsPerPage);
     updateItemsPerPage();
 
-    return () => window.removeEventListener('resize', updateItemsPerPage);
-}, []);
+    return () => window.removeEventListener("resize", updateItemsPerPage);
+  }, []);
 
   return (
     <div className="px-6 pt-6 ml-5 bg-sky-100 h-auto">
-        <SearchBar/>
+      <SearchBar />
       {currentPosts.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-5 min-[1700px]:grid-cols-5 gap-0 ">
           {currentPosts.map((item: ObjectType) => (
@@ -78,23 +110,27 @@ const Products: React.FC = () => {
           <h1>No se encontró lo que buscas, disculpen las molestias.</h1>
         </div>
       )}
-       <div className="flex justify-center space-x-2 pb-4">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 1}
-            className="px-4 py-2 bg-sky-600 text-white rounded-xl disabled:bg-gray-300"
-          >
-           Anterior
-          </button>
-          <p className='text-center justify-center text-base font-semibold mt-2'>{currentPage}</p>
-          <button
-            onClick={nextPage}
-            disabled={currentPage === Math.ceil(FilteredObjects.length / postPerPage)}
-            className="px-4 py-2 bg-sky-600 text-white rounded-xl disabled:bg-gray-300"
-          >
-            Siguiente
-          </button>
-        </div>
+      <div className="flex justify-center space-x-2 pb-4">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="px-4 py-2 bg-sky-600 text-white rounded-xl disabled:bg-gray-300"
+        >
+          Anterior
+        </button>
+        <p className="text-center justify-center text-base font-semibold mt-2">
+          {currentPage}
+        </p>
+        <button
+          onClick={nextPage}
+          disabled={
+            currentPage === Math.ceil(FilteredObjects.length / postPerPage)
+          }
+          className="px-4 py-2 bg-sky-600 text-white rounded-xl disabled:bg-gray-300"
+        >
+          Siguiente
+        </button>
+      </div>
     </div>
   );
 };
