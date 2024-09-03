a8292742d721019f5ceeae7a328abcadf6bece97
juanmeyer94 <juan_meyer@hotmail.com.ar>
2024-09-03 12:54:39 -0300

    price in more info card

--- src/Components/SearchBar/searchBar.tsx
+++ src/Components/SearchBar/searchBar.tsx
@@ -24,9 +24,9 @@ const SearchBar: React.FC = () => {
 
 
   return (
-    <div className="w-full flex">
-      <div className="relative min-w-[18.5%] 2xl:w-[18.5%] lg:w-[22%] md:w-[40%] xs:w-[30%] sm:w-[70%]">
-        <span className="absolute inset-y-0 right-0 flex items-center pr-2">
+    <div className="w-full flex flex-col min-[768px]:flex-row ">
+      <div className="relative min-w-[18.5%] 2xl:w-[18.5%] lg:w-[22%] md:w-[40%] xs:w-[30%] sm:w-[70%] mb-4 sm:ml-6 md:ml-0">
+        <span className="absolute inset-y-0 right-0 flex items-center pr-2 ">
           <img src="/lupa.png" alt="Ready Check" width="24" height="24" />
         </span>
         <input
@@ -37,16 +37,19 @@ const SearchBar: React.FC = () => {
           onChange={handleSearchChange}
         />
       </div>
-      
-        <div className="rounded-xl bg-red-400 text-xs flex items-center ml-2 lg:text-base">
-          <button className="text-white font-semibold px-3 py-1"
-          onClick={resetFilters}>
-            Limpiar filtros
-          </button>
-        </div>
-      
+  
+      <div className="rounded-xl bg-red-400 text-xs flex items-center ml-6 lg:text-base 2xl:ml-7 xl:ml-8 lg:ml-20 md:ml-14 mb-4 sm:w-40 sm:justify-center">
+        <button
+          className="text-white font-semibold px-3 py-1"
+          onClick={resetFilters}
+        >
+          Limpiar filtros
+        </button>
+      </div>
     </div>
   );
+  
+  
 };
 
 
