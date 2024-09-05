882ac6c96eddda695c22494c6a07ea54ec67e7d3
iván Meyer <37188867+IvanMJs@users.noreply.github.com>
2024-09-05 10:00:20 -0300

    jest and rtl config for unit testing component (#14)

---
+++ babel.config.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+//babel.config.cjs
+module.exports = {
+  presets: [
+    '@babel/preset-env',    // Soporta características modernas de JavaScript
+    '@babel/preset-react',  // Soporta JSX y características de React
+    '@babel/preset-typescript', // Soporta TypeScript
+  ],
+};
